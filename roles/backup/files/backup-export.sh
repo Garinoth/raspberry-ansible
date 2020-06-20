f563555c34e19fa73a9d0d67e90dc1cd43794b7c
Garinoth <cdavidrp@gmail.com>
2020-06-20 18:38:27 +0200

    Add backup clean policy and modify frequency

--- roles/backup/files/backup-export.sh
+++ roles/backup/files/backup-export.sh
@@ -2,6 +2,7 @@
 EXTERNAL_DRIVE=/mnt/DAS_1_Blinky
 BACKUP_PATH=backup/`hostname -s`
 ARCHIVE_FILE="$1"
+NUM_FILES_TO_KEEP=12
 
 if [ -z "$1" ]; then
   echo "Please specify the full path of the archive file."
@@ -15,3 +16,6 @@ if mount | grep $EXTERNAL_DRIVE > /dev/null; then
 else
   logger -s "External drive not mounted. Exiting..."
 fi
+
+# Delete old backups
+rm -f `ls -t  $EXTERNAL_DRIVE/$BACKUP_PATH/archive_*.tar.gz | awk "NR>$NUM_FILES_TO_KEEP"`
